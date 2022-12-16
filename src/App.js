23855dd5c0985ea7988461f86f8b6583310154bf
aheadbio <mizukami@ahead-biocomputing.co.jp>
2022-12-17 00:14:50 +0900

    Change the connection destination of the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify, Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>sample react app</h1>
+        <h2>
+          <a className='App-link' href='.' onClick={Auth.signOut}>Sign Out</a>
+        </h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
