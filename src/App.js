2394cd0c3ce6b2923e4804ff840611a18674b498
aheadbio <mizukami@ahead-biocomputing.co.jp>
2022-12-17 14:40:30 +0900

    Authentication fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import '@aws-amplify/ui-react/styles.css';
 import { Amplify, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import aws_exports from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-Amplify.configure(aws_exports);
-
-function App() {
+function App({ signOut }) {
   return (
     <div className="App">
       <header className="App-header">
         <h1>sample react app</h1>
         <h2>
-          <a className='App-link' href='.' onClick={Auth.signOut}>Sign Out</a>
+          {/* <a className='App-link' href='.' onClick={signOut}>
+            Sign Out
+          </a> */}
+          <button onClick={signOut}>Sign out</button>
         </h2>
       </header>
     </div>
